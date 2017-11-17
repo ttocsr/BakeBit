45ee56bf2e340533e1142a46cdab26df39d3b70a
ttocsr <ttocsr@gmail.com>
2017-11-17 10:36:08 -0700

    Update bakebit_nanohat_oled.py
    fix copy paste error

--- Software/Python/bakebit_nanohat_oled.py
+++ Software/Python/bakebit_nanohat_oled.py
@@ -158,13 +158,13 @@ def draw_page():
         IP = get_ip()
         cmd = "dig +short myip.opendns.com @resolver1.opendns.com"
         WANIP = subprocess.check_output(cmd, shell = True )
-        cmd = "iperf3 -c 207.34.75.194 -R -P 4 -t 20 | awk '$NF==\"sender\" {pr$
+        cmd = "iperf3 -c 207.34.75.194 -R -P 4 -t 20 | awk '$NF==\"sender\" {printf \"Download:\", $7, $8; exit}'"
         Download = subprocess.check_output(cmd, shell = True )
-        cmd = "iperf3 -c 207.34.75.194 | awk '$NF==\"sender\" {print \"Up:\",$7$
+        cmd = "iperf3 -c 207.34.75.194 | awk '$NF==\"sender\" {print \"Up:\",$7,$8; exit}'"
         Upload = subprocess.check_output(cmd, shell = True )
 	# Now draw the IP and upload and download. Figure out how to display loading screen while iperf runs.
-        draw.text((x, top+2),       "W IP: " + str(WANIP),  font=smartFont, fil$
-        draw.text((x, top+10),       "L IP: " + str(IP),  font=smartFont, fill=$
+        draw.text((x, top+2),       "W IP: " + str(WANIP),  font=smartFont, fill=255)
+        draw.text((x, top+10),       "L IP: " + str(IP),  font=smartFont, fill=255)
         draw.text((x, top+5+20),     str(Download), font=font11, fill=255)
         draw.text((x, top+5+36),    str(Upload),  font=font11, fill=255)
     elif page_index==3: #shutdown -- no
